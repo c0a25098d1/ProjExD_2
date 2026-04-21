575c9c8fd0e00a93102bf5efa05abfa373caf405
kanta <c0a25098d1@edu.teu.ac.jp>
2026-04-21 14:45:53 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,6 +6,18 @@ import random
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数: こうかとんRectかばくだんRect
+    戻り値: タプル (横方向判定結果, 縦方向判定結果)
+    画面内ならTrue, 画面外ならFalse
+    """
+    yoko, tate = True,True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: #横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: #縦方向判定
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -19,10 +31,13 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0, 1100), random.randint(0, 600)
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = 5, 5
 
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -31,8 +46,6 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        vx = 5
-        vy = 5
         DELTA = {pg.K_UP:(0, -5),pg.K_DOWN:(0, +5),pg.K_LEFT:(-5, 0),pg.K_RIGHT:(+5, 0)}
         if key_lst[pg.K_UP]:
             sum_mv= DELTA[pg.K_UP]
@@ -43,8 +56,15 @@ def main():
         if key_lst[pg.K_RIGHT]:
             sum_mv= DELTA[pg.K_RIGHT]
         kk_rct.move_ip(sum_mv)
-        bb_rct.move_ip((vx, vy))
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip((vx, vy))
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
